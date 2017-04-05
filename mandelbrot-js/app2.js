d356ce0f4b977971cac8dc17ff36cb3458d9d495
Ilya Sedlovsky <isedlovsky@griddynamics.com>
2017-04-05 20:21:53 +0300

    Mandelbrot-js fixes

--- mandelbrot-js/app2.js
+++ mandelbrot-js/app2.js
@@ -1,3 +1,9 @@
+/* global Workerp:false */
+/* global Complex:false */
+/* global Color:false */
+/* global Random:false */
+/* global Hammer:false */
+
 (function () {
   var WORKERS = navigator.hardwareConcurrency || 4;
   var workers = [];
@@ -16,16 +22,18 @@
   var c1;
   var c2;
   var c0;
-  var xm = 0, ym = 0;
+  var mouseX = 0, mouseY = 0;
   var stepsValues = [255, 500, 1000, 2000, 4000, 8000];
   var stepsValuesIdx = 0;
   var steps = stepsValues[0];
   var running = false;
-  var averageTileCalcTime = 100;
-  var drawCount = 0;
+  var averageTileCalcTime = 0;
   var palete;
   var random = new Random();
   var rebuildPalete = buildFractalPalete;
+  var ctrlPressed = false;
+  var resetMouseCoords = false;
+  var prevHiehgt, prevWidth;
   //var rebuildPalete = buildPalete;
 
   init(true);
@@ -37,9 +45,9 @@
       prevHiehgt = c.height;
     }
 
-    c.width  = window.innerWidth;
+    c.width = window.innerWidth;
     c.height = window.innerHeight;
-    ctx = c.getContext("2d", {alpha: false});
+    ctx = c.getContext("2d", { alpha: false });
 
     width = c.width;
     height = c.height;
@@ -53,7 +61,7 @@
     var re1, re2, im1, im2;
     var f = width / height;
     if (c1 && c2) {
-      var center = new Complex((c1.re + c2.re)/2, (c1.im + c2.im)/2);
+      var center = new Complex((c1.re + c2.re) / 2, (c1.im + c2.im) / 2);
       re1 = (c1.re - center.re) * width / prevWidth + center.re;
       im1 = (c1.im - center.im) * height / prevHiehgt + center.im;
       re2 = (c2.re - center.re) * width / prevWidth + center.re;
@@ -66,7 +74,7 @@
         re2 = im2 * f;
       } else {
         re1 = -2;
-        re2 =  2;
+        re2 = 2;
         im1 = re1 / f;
         im2 = re2 / f;
       }
@@ -83,12 +91,11 @@
   function drawSet(c1, c2, ctx) {
     saveState();
     stopCalculations();
-    drawCount++;
 
     Promise.all(workers.map(function (worker) {
       return worker.call({
         palete: palete
-      })
+      });
     })).then(function () {
       var parts = [];
 
@@ -96,7 +103,7 @@
       for (var x = 0; x < width; x += tileSize) {
         for (var y = 0; y < height; y += tileSize) {
           var x2 = Math.min(x + tileSize - 1, width - 1);
-          var y2 = Math.min(y + tileSize -1, height -1);
+          var y2 = Math.min(y + tileSize - 1, height - 1);
           parts.push([[x, y], [x2, y2]]);
         }
       }
@@ -105,54 +112,54 @@
       parts.sort(function (a, b) {
         var xa = (a[0][0] + a[1][0]) / 2;
         var ya = (a[0][1] + a[1][1]) / 2;
-        var distA = (xm - xa)*(xm - xa) + (ym - ya)*(ym - ya);
+        var distA = (mouseX - xa) * (mouseX - xa) + (mouseY - ya) * (mouseY - ya);
 
         var xb = (b[0][0] + b[1][0]) / 2;
         var yb = (b[0][1] + b[1][1]) / 2;
-        var distB = (xm - xb)*(xm - xb) + (ym - yb)*(ym - yb);
+        var distB = (mouseX - xb) * (mouseX - xb) + (mouseY - yb) * (mouseY - yb);
 
         return distA < distB ? -1 : distA == distB ? 0 : 1;
       });
 
+      var partsFinished = 0;
+      averageTileCalcTime = 0;
       running = true;
       updateProgressIndicator();
       parts.forEach(function (part) {
         computePart(part[0][0], part[0][1], part[1][0], part[1][1]);
       });
 
-      var partsFinished = 0;
       function computePart(x1, y1, x2, y2) {
-        var startTime = new Date();
-
         worker().call({
-          x1: x1, y1: y1, 
-          x2: x2, y2: y2, 
-          width: width, height: height, 
+          x1: x1, y1: y1,
+          x2: x2, y2: y2,
+          width: width, height: height,
           c1: c1, c2: c2, c0: c0,
-          steps: steps})
-        .then(function (imd) {
-          partsFinished++;
-          running = partsFinished < parts.length;
-          updateProgressIndicator();
-          var width = x2 - x1 + 1;
-          var height =  y2 - y1 + 1;
+          steps: steps
+        })
+          .then(function (results) {
+            var imd = results.imd;
+            var renderTime = results.renderTime;
 
-          ctx.putImageData(imd, x1, y1);
-          averageTileCalcTime = (averageTileCalcTime + (new Date()).getTime() - startTime.getTime()) / 2;
-          /*var bp = createImageBitmap(imd, 0, 0, width, height).then(function (image) {
-            ctx.drawImage(image, x1, y1);
-            averageTileCalcTime = (averageTileCalcTime + (new Date()).getTime() - startTime.getTime()) / 2;
-          });*/
-        });
-      }      
+            partsFinished++;
+            running = partsFinished < parts.length;
+            if (!running) {
+              //console.debug("Render time", ((new Date()).getTime() - renderStartTime.getTime()) / 1000);
+            }
+            updateProgressIndicator();
+
+            ctx.putImageData(imd, x1, y1);
+            averageTileCalcTime = (averageTileCalcTime + renderTime) / 2;
+          });
+      }
     });
   }
 
-  function buildPalete(steps) {
-    var zoom = Math.round(Math.log2(1/Math.abs(c1.re - c2.re)));
+  /*function buildPalete(steps) {
+    var zoom = Math.round(Math.log2(1 / Math.abs(c1.re - c2.re)));
     zoom = zoom < 1 ? 1 : zoom;
     var k = 1 - Math.pow(2, -zoom);
-    colors = [];
+    var colors = [];
     for (var s = 0; s <= steps; s++) {
       var d = 1 - s / steps;
 
@@ -160,40 +167,40 @@
       //colors[s] = Color.fromHsv(1, d/255, 1 - d/255);
 
       var vlog = Math.log2(d + 1);
-      var vexp = Math.pow(zoom*5 + 1, d) / (zoom*5 + 1);
-      var v = vlog*k + vexp * (1 - k);
+      var vexp = Math.pow(zoom * 5 + 1, d) / (zoom * 5 + 1);
+      var v = vlog * k + vexp * (1 - k);
       //var v = 255 * d;
       //colors[s] = {r: 255*v, g: 255*v, b: 255*v};
 
       colors[s] = Color.fromHsv(d, v, v);
     }
     return colors;
-  }
+  }*/
 
   function buildFractalPalete(steps) {
     var colors = [];
-    
+
     colors[0] = new Color(255, 255, 255);
     colors[steps] = new Color(0, 0, 0);
 
     var buildFractalPaleteR = function (i1, i2, level) {
       if (i2 - i1 < 2) return;
 
-      var rand = rand = (2*random.nextFloat(0, 1) - 1) * 255 / level;
-      var r = Math.round( (colors[i1].r + colors[i2].r) / 2 + rand );
+      var rand = rand = (2 * random.nextFloat(0, 1) - 1) * 255 / level;
+      var r = Math.round((colors[i1].r + colors[i2].r) / 2 + rand);
 
-      rand = (2*random.nextFloat(0, 1) - 1) * 255 / level;
-      var g = Math.round( (colors[i1].g + colors[i2].g) / 2 + rand );
+      rand = (2 * random.nextFloat(0, 1) - 1) * 255 / level;
+      var g = Math.round((colors[i1].g + colors[i2].g) / 2 + rand);
 
-      rand = rand = (2*random.nextFloat(0, 1) - 1) * 255 / level;
-      var b = Math.round( (colors[i1].b + colors[i2].b) / 2 + rand );
+      rand = rand = (2 * random.nextFloat(0, 1) - 1) * 255 / level;
+      var b = Math.round((colors[i1].b + colors[i2].b) / 2 + rand);
 
-      var imid = Math.round( (i1 + i2)/2 );
+      var imid = Math.round((i1 + i2) / 2);
       colors[imid] = new Color(r, g, b);
 
       buildFractalPaleteR(i1, imid, level * 1.7);
       buildFractalPaleteR(imid, i2, level * 1.7);
-    }
+    };
 
     buildFractalPaleteR(0, steps, 1);
     return colors;
@@ -203,22 +210,22 @@
     var w = 64;
     var h = 64;
 
-    var canvas = document.createElement('canvas');
+    var canvas = document.createElement("canvas");
     canvas.width = w;
     canvas.height = h;
-    var ctx = canvas.getContext('2d');
+    var ctx = canvas.getContext("2d");
     ctx.drawImage(sourceContext, 0, 0, width, height, 0, 0, w, h);
 
-    var link = document.getElementById('favicon');
+    var link = document.getElementById("favicon");
     link.href = canvas.toDataURL("image/x-icon");
-    document.getElementsByTagName('head')[0].appendChild(link);
+    document.getElementsByTagName("head")[0].appendChild(link);
   }
 
   function saveState() {
     var newState = btoa(JSON.stringify({
       center: {
-        re: (c1.re + c2.re)/2,
-        im: (c1.im + c2.im)/2
+        re: (c1.re + c2.re) / 2,
+        im: (c1.im + c2.im) / 2
       },
       dist: c2.re - c1.re,
       stepsValuesIdx: stepsValuesIdx,
@@ -235,7 +242,7 @@
     try {
       var state = JSON.parse(atob(document.location.hash.substr(1)));
       var center = state.center;
-      var w = state.dist/2;
+      var w = state.dist / 2;
       var h = state.dist * height / width / 2;
       var re1 = center.re - w;
       var im1 = center.im - h;
@@ -257,7 +264,9 @@
       }
 
       return true;
-    } catch (e) {}
+    } catch (e) {
+      // just ignore if something went wrong while deserializing the state
+    }
 
     return false;
   }
@@ -272,30 +281,38 @@
 
   var moving = false;
   var juliaMoving = false;
-  window.onmousedown = function(e) {
+  var x1, y1;
+  window.onmousedown = function (e) {
     if (moving || juliaMoving) return;
+    x1 = e.offsetX;
+    y1 = e.offsetY;
     if (e.shiftKey) {
       juliaMoving = true;
       moving = false;
     } else {
       juliaMoving = false;
       moving = true;
-      x1 = e.offsetX;
-      y1 = e.offsetY;
     }
   };
 
   var debouncedDrawSet = debounce(drawSet, 100);
-  window.onmousemove = function(e) {
-    xm = e.offsetX;
-    ym = e.offsetY;
+  window.onmousemove = function (e) {
+    mouseX = e.offsetX;
+    mouseY = e.offsetY;
+
+    var x2 = e.offsetX;
+    var y2 = e.offsetY;
+
+    if (resetMouseCoords) {
+      x1 = x2;
+      y1 = y2;
+      resetMouseCoords = false;
+      return;
+    }
 
     if (moving) {
-      x2 = e.offsetX;
-      y2 = e.offsetY;
-
       var before = Complex.fromImage(x1, y1, c1, c2, width, height);
-      var now    = Complex.fromImage(x2, y2, c1, c2, width, height);
+      var now = Complex.fromImage(x2, y2, c1, c2, width, height);
 
       c1.im -= now.im - before.im;
       c1.re -= now.re - before.re;
@@ -308,6 +325,18 @@
 
       debouncedDrawSet(c1, c2, ctx);
     } else if (juliaMoving) {
+      var xm, ym;
+      var dx = x2 - x1;
+      var dy = y2 - y1;
+
+      if (ctrlPressed) {
+        xm = x1 + dx / 4;
+        ym = y1 + dy / 4;
+      } else {
+        xm = x2;
+        ym = y2;
+      }
+
       c0 = Complex.fromImage(xm, ym, c1, c2, width, height);
       debouncedDrawSet(c1, c2, ctx);
     }
@@ -316,35 +345,36 @@
   function debounce(func, swallowInterval) {
     var swallowTimeout;
 
-    return function() {
+    return function () {
       var context = this, args = arguments;
 
-      var later = function() {
+      var later = function () {
         clearTimeout(swallowTimeout);
         swallowTimeout = null;
 
         func.apply(context, args);
       };
 
-      if (!swallowTimeout) {
-        swallowTimeout = setTimeout(later, swallowInterval);
+      if (swallowTimeout) {
+        clearTimeout(swallowTimeout);
       }
+      swallowTimeout = setTimeout(later, swallowInterval);
     };
-  };
+  }
 
-  window.onmouseup = function(e) {
+  window.onmouseup = function () {
     moving = false;
     juliaMoving = false;
   };
 
   var mousewheelevt = (/Firefox/i.test(navigator.userAgent))
-    ? "DOMMouseScroll" 
+    ? "DOMMouseScroll"
     : "mousewheel";
-   
+
   if (window.attachEvent)
-      window.attachEvent("on" + mousewheelevt, onmousewheel);
+    window.attachEvent("on" + mousewheelevt, onmousewheel);
   else if (window.addEventListener)
-      window.addEventListener(mousewheelevt, onmousewheel, false)
+    window.addEventListener(mousewheelevt, onmousewheel, false);
 
   function onmousewheel(e) {
     var center = Complex.fromImage(e.offsetX, e.offsetY, c1, c2, width, height);
@@ -354,8 +384,8 @@
     var zoomIn = e.detail ? e.detail < 0 : e.deltaY < 0;
     if (zoomIn) {
       // zoom in
-      c1New = new Complex((c1.re - xc) / 2 + xc, (c1.im - yc) / 2 + yc);
-      c2New = new Complex((c2.re - xc) / 2 + xc, (c2.im - yc) / 2 + yc);
+      var c1New = new Complex((c1.re - xc) / 2 + xc, (c1.im - yc) / 2 + yc);
+      var c2New = new Complex((c2.re - xc) / 2 + xc, (c2.im - yc) / 2 + yc);
     } else {
       // zoom out
       c1New = new Complex((c1.re - xc) * 2 + xc, (c1.im - yc) * 2 + yc);
@@ -379,12 +409,12 @@
     }
 
     drawSet(c1, c2, ctx);
-  }
+  };
 
   function status() {
-    var zoom = Math.round(Math.log2(1/Math.abs(c1.re - c2.re)));
-    document.getElementById("div").innerHTML = 
-        "<b>" + (c0 ? "Julia Set" : "Mandelbrot Set") + "</b>"
+    var zoom = Math.round(Math.log2(1 / Math.abs(c1.re - c2.re)));
+    document.getElementById("div").innerHTML =
+      "<b>" + (c0 ? "Julia Set" : "Mandelbrot Set") + "</b>"
       + "<br />Zoom: " + zoom
       + "<br />Steps: " + steps
       + "<br />TileRender: " + Math.round(averageTileCalcTime) + "ms";
@@ -395,19 +425,29 @@
   function updateProgressIndicator() {
     document.getElementById("progress").style.display = running ? "block" : "none";
     if (!running) {
-      renderFavicon(canvas);
+      renderFavicon(document.getElementById("canvas"));
     }
   }
 
-  window.onresize = function (e) {
+  window.onresize = function () {
     init();
-  }
+  };
 
   window.onkeyup = function (e) {
     if (e.keyCode == 27) {
       reset();
+    } else if (e.keyCode == 17) {
+      ctrlPressed = false;
+      resetMouseCoords = true;
     }
-  }
+  };
+
+  window.onkeydown = function (e) {
+    if (e.keyCode == 17) {
+      ctrlPressed = true;
+      resetMouseCoords = true;
+    }
+  };
 
   window.onkeypress = function (e) {
     var ch = String.fromCharCode(e.charCode);
@@ -426,22 +466,22 @@
   }
 
   function cycleDetails() {
-    stepsValuesIdx++
+    stepsValuesIdx++;
     steps = stepsValues[stepsValuesIdx % stepsValues.length];
     palete = rebuildPalete(steps);
     drawSet(c1, c2, ctx);
   }
 
-  window.onpopstate = function (e) {
+  window.onpopstate = function () {
     loadState();
     drawSet(c1, c2, ctx);
   };
 
-  var hammertime = new Hammer(canvas, {});
-  hammertime.get('pinch').set({ enable: true });
-  hammertime.get('rotate').set({ enable: true });
+  var hammertime = new Hammer(document.body, {});
+  hammertime.get("pinch").set({ enable: true });
+  hammertime.get("rotate").set({ enable: true });
 
-  hammertime.on('pinchend', function (e) {
+  hammertime.on("pinchend", function (e) {
     onmousewheel({
       offsetX: e.center.x,
       offsetY: e.center.y,
@@ -449,21 +489,21 @@
     });
   });
 
-  hammertime.on('panstart', function (e) {
+  hammertime.on("panstart", function (e) {
     window.onmousedown({
       offsetX: e.center.x,
       offsetY: e.center.y
     });
   });
 
-  hammertime.on('panmove', function(e) {
+  hammertime.on("panmove", function (e) {
     window.onmousemove({
       offsetX: e.center.x,
       offsetY: e.center.y
     });
   });
 
-  hammertime.on('panend', function (e) {
+  hammertime.on("panend", function (e) {
     window.onmouseup({
       offsetX: e.center.x,
       offsetY: e.center.y
