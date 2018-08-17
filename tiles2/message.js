0487a84edf954e9d52f21db0eb900392756f7a68
isedlovsky <isedlovsky@griddynamics.com>
2018-08-17 15:55:02 +0300

    message overlay css fix

--- tiles2/message.js
+++ tiles2/message.js
@@ -1,19 +1,7 @@
 "use strict";
 
 var Message = (function () {
-  var messageDiv = document.createElement("div");
-  messageDiv.style.position = "absolute";
-  messageDiv.style.left = "0";
-  messageDiv.style.top = "0";
-  messageDiv.style.width = "100%";
-  messageDiv.style.height = "100%";
-  messageDiv.style.fontSize = "20pt";
-  messageDiv.style.display = "none";
-  messageDiv.style.backgroundColor = "white";
-  messageDiv.style.textAlign = "center";
-  messageDiv.style.verticalAlign = "middle";
-  messageDiv.style.lineHeight = "300px";
-  document.body.appendChild(messageDiv);
+  var messageDiv = document.getElementById("message");
 
   function showMessage(message) {
     !message || (messageDiv.innerHTML = message);
