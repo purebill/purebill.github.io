bb8ba5aa906f5427f43531b2f0c385bb414164c3
Ilya Sedlovsky <isedlovsky@griddynamics.com>
2021-02-13 11:16:46 +0200

    Balls

--- balls/collider.js
+++ balls/collider.js
@@ -14,7 +14,7 @@ const collideChecksMetrics = Metrics.gauge("collide_checks");
 export function collider(balls, tree, w, h) {
   let checks = 0;
   let updated = balls.map(b => 0);
-  let balls2 = balls.map(b => [0, 0]);
+  let nextv = balls.map(b => [0, 0]);
 
   for (let i = 0; i < balls.length; i++) {
     const b1 = balls[i];
@@ -27,40 +27,40 @@ export function collider(balls, tree, w, h) {
         const d = V.subtract(b1.p, b2.p);
         if (V.project(v2, d) > 0) {
           const [newv1, newv2] = collide(b1, b2);
-          balls2[i][0] += newv1[0];
-          balls2[i][1] += newv1[1];
-          balls2[j][0] += newv2[0];
-          balls2[j][1] += newv2[1];
+          nextv[i][0] += newv1[0];
+          nextv[i][1] += newv1[1];
+          nextv[j][0] += newv2[0];
+          nextv[j][1] += newv2[1];
           updated[i]++;
           updated[j]++;
         }
       }
     }
 
-    let v = updated[i] ? balls2[i] : b1.v;
+    let v = updated[i] ? nextv[i] : b1.v;
 
     if (b1.p[0] - b1.r <= 0 && v[0] < 0 || b1.p[0] + b1.r >= w && v[0] > 0) {
       if (updated[i]) {
-        balls2[i][0] = -balls2[i][0];
+        nextv[i][0] = -nextv[i][0];
       } else {
-        balls2[i][0] = -b1.v[0];
-        balls2[i][1] = b1.v[1];
+        nextv[i][0] = -b1.v[0];
+        nextv[i][1] = b1.v[1];
         updated[i]++;
       }
     }
     if (b1.p[1] - b1.r <= 0 && v[1] < 0 || b1.p[1] + b1.r >= h && v[1] > 0) {
       if (updated[i]) {
-        balls2[i][1] = -balls2[i][1];
+        nextv[i][1] = -nextv[i][1];
       } else {
-        balls2[i][0] = b1.v[0];
-        balls2[i][1] = -b1.v[1];
+        nextv[i][0] = b1.v[0];
+        nextv[i][1] = -b1.v[1];
         updated[i]++;
       }
     }
   }
   
-  for (let i = 0; i < balls2.length; i++) {
-    if (updated[i]) balls[i].v = balls2[i];
+  for (let i = 0; i < nextv.length; i++) {
+    if (updated[i]) balls[i].v = nextv[i];
     // if (updated[i] > 1) paused = true;
   }
 
